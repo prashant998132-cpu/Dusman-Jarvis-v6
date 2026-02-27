645f654c31ebd1a0e5aa0afabfa80b94b3d55444
prashant998132-cpu <prashant998132@gmail.com>
2026-02-27 16:26:13 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = 'jarvis-v6'
+const STATIC = ['/', '/manifest.json']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ))
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok && e.request.method === 'GET')
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()))
+        return res
+      })
+      return cached || network
+    })
+  )
+})
